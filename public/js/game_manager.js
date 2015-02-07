8208b5d9c21b5c8ad2d2e40df06aa537ea02ac7e
nglinh <nglinh2312@gmail.com>
2015-02-08 05:08:07 +0800

    Fixed typo in game_manager

--- public/js/game_manager.js
+++ public/js/game_manager.js
@@ -1,4 +1,4 @@
-bfunction GameManager(size, InputManager, Actuator, StorageManager) {
+function GameManager(size, InputManager, Actuator, StorageManager) {
   this.size           = size; // Size of the grid
   this.inputManager   = new InputManager;
   this.storageManager = new StorageManager;
