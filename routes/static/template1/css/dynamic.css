40ff661c6407bcbf46feb61a19e64dd96561017a
stewartSG <stewart.addmein@gmail.com>
2015-02-07 13:14:25 +0800

    Adding template 1

---
+++ routes/static/template1/css/dynamic.css
@@ -0,0 +1,114 @@
+/* BEGIN custom_css */
+#portfolio .portfolio-row .portfolio-mix .portfolio-item a .caption-bg {
+-webkit-box-shadow: inset 0 0 0 6px #DF5757, inset 0 0 0 0 #7b73d6;
+-moz-box-shadow: inset 0 0 0 6px #DF5757, inset 0 0 0 0 #7b73d6;
+box-shadow: inset 0 0 0 6px #DF5757, inset 0 0 0 0 #7b73d6;
+}
+#portfolio .portfolio-row .portfolio-mix .portfolio-item a .caption-bg::before {
+content: '';
+-webkit-box-shadow: inset 0 0 0 0 #7b73d6;
+-moz-box-shadow: inset 0 0 0 0 #7b73d6;
+box-shadow: inset 0 0 0 0 #7b73d6;
+}
+#portfolio .portfolio-row .portfolio-mix .portfolio-item a:hover .caption-bg::before {
+content: '';
+-webkit-box-shadow: inset 0 0 0 160px #7b73d6;
+-moz-box-shadow: inset 0 0 0 160px #7b73d6;
+box-shadow: inset 0 0 0 160px #7b73d6;
+}
+/* END custom_css */
+/* BEGIN custom_css_theme */
+#portfolio .portfolio-row .portfolio-mix .portfolio-item a .caption-bg {
+-webkit-box-shadow: inset 0 0 0 6px #DF5757, inset 0 0 0 0 #579F6A;
+-moz-box-shadow: inset 0 0 0 6px #DF5757, inset 0 0 0 0 #579F6A;
+box-shadow: inset 0 0 0 6px #DF5757, inset 0 0 0 0 #579F6A;
+}
+#portfolio .portfolio-row .portfolio-mix .portfolio-item a .caption-bg::before {
+content: '';
+-webkit-box-shadow: inset 0 0 0 0 #579F6A;
+-moz-box-shadow: inset 0 0 0 0 #579F6A;
+box-shadow: inset 0 0 0 0 #579F6A;
+}
+#portfolio .portfolio-row .portfolio-mix .portfolio-item a:hover .caption-bg::before {
+content: '';
+-webkit-box-shadow: inset 0 0 0 160px #579F6A;
+-moz-box-shadow: inset 0 0 0 160px #579F6A;
+box-shadow: inset 0 0 0 160px #579F6A;
+}
+body {  }
+h1 {  }
+h2 {  }
+h3 {  }
+h4 {  }
+h5 {  }
+.jumbotron h1 {  color: ; }
+.navbar, .single-top-menu {  }
+.service-column:hover { background-color: #579F6A; }
+.service-column:hover i { color: #579F6A; }
+.service-column:hover h2, .service-column:hover p { color: #ffffff; }
+.service-column:hover .service-icon { 
+-webkit-box-shadow: inset 0 0 0 0 #579F6A;
+-moz-box-shadow: inset 0 0 0 0 #579F6A;
+box-shadow: inset 0 0 0 0 #579F6A;
+}
+.jumbotron p, .jumbotron p .btn:hover { color: ; }
+
+.navbar .navbar-nav .sub-menu li a:hover,
+#about .social-icons .icon-social i,
+#portfolio .filter.btn-outline-white,
+.post-title a:hover,
+.single-post-meta i,
+.comment-respond .logged-in-as a,
+.form-submit input,
+.primary-menu li ul li a:hover,
+.primary-menu li ul .current-menu-item a,
+#portfolio .btn-red,
+#blog .btn-red { color: ; }
+
+#about .round-outline .round-photo-anchor .round-caption-bg,
+#about .social-icons .icon-social:hover,
+#portfolio .filter.btn-outline-white:hover,
+#portfolio .cat-list li.active,
+#portfolio .cat-list li:active,
+.da-dots span.da-dots-current:after,
+.form-submit input:hover,
+.primary-menu li a:hover,
+.primary-menu .current-menu-item a,
+.navbar .navbar-nav li a:hover,
+.navbar .navbar-nav li:hover,
+.navbar .navbar-nav .active,
+.bx-wrapper .bx-pager.bx-default-pager .active:after,
+#portfolio .btn-red:hover,
+#portfolio .btn-red:focus,
+#blog .btn-red:hover,
+#blog .btn-red:focus { background-color: ; }
+
+#about .social-icons .icon-social,
+#portfolio .filter.btn-outline-white,
+.da-slide .da-img .round-outline,
+.form-submit input,
+#portfolio .btn-red,
+#blog .btn-red { border-color: ; }
+
+#portfolio .btn-red:hover,
+#portfolio .btn-red:focus { color: #fff; }
+#blog .btn-red:hover,
+#blog .btn-red:focus { color: #fff; }
+
+#about h1 { color: ; }
+#about { color: ; }
+#services h1 { color: ; }
+#services { color: ; }
+#testimonials h1 { color: ; }
+#testimonials, #testimonials blockquote { color: ; }
+#blog h1 { color: ; }
+#contact h1 { color: ; }
+#contact, #contact p, #contact .icon, #contact form input, #contact form textarea { color: ; }
+#contact form input::-webkit-input-placeholder,
+#contact form textarea::-webkit-input-placeholder { color: ; }
+#contact form input:-moz-placeholder,
+#contact form textarea:-moz-placeholder { color: ; }
+#contact form button[type='submit'] { color: ; border-color: ; background: none;}
+#contact form button[type='submit']:hover { background-color: ; }
+.footer { color: ; }
+/* END custom_css_theme */
