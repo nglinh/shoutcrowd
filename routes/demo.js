3205ce0f54bbbb489a1c897f9f2717490ba02c4a
stewartSG <stewart.addmein@gmail.com>
2015-02-07 13:09:18 +0800

    Changing fb destination

--- routes/demo.js
+++ routes/demo.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('demo', { title: 'Express' });
-  var myFirebaseRef = new Firebase("https://<your-firebase>.firebaseio.com/");
+  var myFirebaseRef = new Firebase("https://fbhack.firebaseIO.com");
 });
 
 module.exports = router;
