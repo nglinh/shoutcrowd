f2fa9a4e5248cb7fd166a926ce210ee1702ba874
Nguyen Ngoc Trung <trunga0@gmail.com>
2015-02-07 23:06:52 +0800

    Fixed game related function

--- public/javascripts/presenter.js
+++ public/javascripts/presenter.js
@@ -2,19 +2,21 @@ var firebase = new Firebase("https://fbhack.firebaseio.com");
 
 window.requestAnimationFrame(function () {
 
+
+
+    var isFullyInitialized = false;
     window.gameManager = new GameManager(4, VoiceInputManager, HTMLActuator, LocalStorageManager);
-// listen for changes and use the forceUpdate function on GameManage
+// // listen for changes and use the forceUpdate function on GameManage
     firebase.child("gameStates").on("value", function(snapshot){
         console.log(snapshot.val());
-        window.gameManager.forceUpdate(snapshot.val());
+        if (!isFullyInitialized) {
+            window.gameManager.forceUpdate(snapshot.val());
+        }
+        isFullyInitialized = true;
       }, function (errorObject){
         console.log("failed to get new gameState: " + errorObject.code);
     });
 
-    // setTimeout(function() {
-    //     window.gameManager.moveUp();
-    //     firebase.child("gameStates").set(window.gameManager.toAPIObject());
-    // }, 5000);
 
     firebase.child("serverCommand").on("value", function(command) {
         // console.log(command.val().command);
